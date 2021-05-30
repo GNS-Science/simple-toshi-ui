848359d3383fbfb3b339737aaed8a6be414e3b67
Wooneh <sam.woon@hotmail.com>
2021-05-30 14:02:43 +1200

    reduce max length

--- src/components/TruncateText.tsx
+++ src/components/TruncateText.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  * Truncates text to a given length.
  */
-const truncate = (text: string, maxLength = 100): string => {
+const truncate = (text: string, maxLength = 60): string => {
   if (text.length > maxLength) {
     const sideLength = Math.floor(maxLength / 2);
     const left = text.substr(0, sideLength);
