fd1f34154414f388444cdf0cee40761c7fc83d76
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-06-09 09:32:59 +1200

    tidy up the now working PreviewLineMFD component

--- src/components/PreviewLineMFD.tsx
+++ src/components/PreviewLineMFD.tsx
@@ -2,76 +2,99 @@ import * as d3 from 'd3';
 import React, { useRef, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
 
-import { d0, magRateData, IMagRate } from './PreviewMFD_data';
+import { d0, d1, magRateData, IMagRate } from './PreviewMFD_data';
 
 const PreviewMFD: React.FC = () => {
   const ref = useRef<SVGSVGElement>(null);
-  const data = magRateData(d0); //[10, 40, 30, 20, 50, 10];
+  const supraData = magRateData(d0); //[10, 40, 30, 20, 50, 10];
+  const subData = magRateData(d1);
   // const data = d0; //[10, 40, 30, 20, 50, 10];
-  const maxData = d3.max(data.map((x) => x.rate)) || 0;
-  const width = 800;
-  const height = 600;
+  // const maxData = d3.max(supraData.map((x) => x.rate)) || 0;
+
+  const margin = { top: 20, right: 10, bottom: 20, left: 50 },
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
   useEffect(() => {
-    d3.select(ref.current).attr('width', width).attr('height', height).style('border', '1px solid black');
-  }, []);
+    d3.select(ref.current)
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .style('border', '1px solid black')
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+  }, [height, margin.bottom, margin.left, margin.right, margin.top, width]);
 
   useEffect(() => {
     draw();
-  }, [data]);
+  }, [supraData, subData]);
 
   const draw = () => {
     const svg = d3.select(ref.current);
-    console.log('DATA', data);
-    const x = d3.scaleLinear().domain([5.0, 9.0]).range([0, width]);
+    // console.log('DATA', supraData);
 
+    // X axis
+    // prettier-ignore
+    const x = d3.scaleLinear()
+      .domain([5.0, 8.9])
+      .range([margin.left, width]);
     svg
       .append('g')
       .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(x));
 
     // Add Y axis
-    const y = d3.scaleLog().domain([10e-9, maxData]).range([height, 0]);
-    svg.append('g').call(d3.axisLeft(y));
-
-    //Add the line
     // prettier-ignore
+    const y = d3.scaleLog()
+      .domain([1e-9, 0.2])
+      .range([height, 0]);
     svg
-      .append<SVGPathElement>('path')
-      .datum(data)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ')')
+      .call(d3.axisLeft(y));
+
+    // Add the supra line
+    // prettier-ignore
+    svg.append<SVGPathElement>('path')
+      .datum(supraData)
       .attr('fill', 'none')
       .attr('stroke', 'steelblue')
-      .attr('stroke-width', 1.5);
-    // @wooneh I could not get this to work in typescript, maybe you can :)
-    //   .attr('d', d3.line()
-    //     .x((d) => x(d.mag))
-    //     .y((d) => y(d.rate))
-    // );
+      .attr('stroke-width', 3)
+      .attr('d', d3.line<IMagRate>()
+          .x((d) => x(d.mag))
+          .y((d) => y(d.rate)),
+      );
 
-    // // hint from https://stackoverflow.com/a/40767846
-    // // prettier-ignore
-    // const mySelection: d3.Selection<SVGElement, unknown, HTMLElement, any> = d3.selectAll<SVGElement, unknown>('g');
-    // function foo(): d3.Selection<SVGPathElement, unknown, HTMLElement, any> {
-    //   return mySelection.append<SVGPathElement>('g');
-    // }
-    // console.log(foo());
-    // // alert(foo().node().tagName || "nothing")
+    // Add line 2, subSeismo
+    // prettier-ignore
+    svg.append<SVGPathElement>('path')
+      .datum(subData)
+      .attr('fill', 'none')
+      .attr('stroke', 'orange')
+      .attr('stroke-width', 3)
+      .attr('d', d3.line<IMagRate>()
+          .x((d) => x(d.mag))
+          .y((d) => y(d.rate))
+    );
 
     // Handmade legend
-    svg.append('circle').attr('cx', 450).attr('cy', 30).attr('r', 6).style('fill', 'orange');
-    svg.append('circle').attr('cx', 450).attr('cy', 60).attr('r', 6).style('fill', '#404080');
-    svg.append('text').attr('x', 470).attr('y', 36).text('variable A').style('font-size', '15px');
+    const w0 = width - 100,
+      w1 = width - 80;
+    svg.append('circle').attr('cx', w0).attr('cy', 30).attr('r', 6).style('fill', 'orange');
+    svg.append('circle').attr('cx', w0).attr('cy', 60).attr('r', 6).style('fill', 'steelblue');
+    svg.append('text').attr('x', w1).attr('y', 36).text('sub seismogenic').style('font-size', '15px');
     svg.attr('alignment-baseline', 'middle');
-    svg.append('text').attr('x', 470).attr('y', 66).text('variable B').style('font-size', '15px');
+    svg.append('text').attr('x', w1).attr('y', 66).text('supra seismogenic').style('font-size', '15px');
     svg.attr('alignment-baseline', 'middle');
   };
 
   return (
     <>
-      <Typography variant="h5" gutterBottom>
-        Wairarapa
+      <Typography variant="h4" gutterBottom>
+        Named fault system: Wairarapa
       </Typography>
-
+      <Typography variant="h6" gutterBottom>
+        Incremental participation rate by magnitude
+      </Typography>
       <div className="chart">
         <svg ref={ref}></svg>
       </div>
