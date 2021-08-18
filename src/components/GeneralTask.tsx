be23bf16bc7cdff980d5cb05a0259afff941c316
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-08-18 16:10:55 +1200

    on demand (via accordion); added the new simple fields;

--- src/components/GeneralTask.tsx
+++ src/components/GeneralTask.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { graphql } from 'babel-plugin-relay/macro';
 import { useParams } from 'react-router-dom';
 import { useLazyLoadQuery } from 'react-relay/hooks';
-import { List, ListItem, ListItemText, Typography } from '@material-ui/core';
+import { List, ListItem, ListItemText, Typography, CircularProgress } from '@material-ui/core';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import { GeneralTaskQuery } from './__generated__/GeneralTaskQuery.graphql';
 import { format } from 'date-fns';
 import ReactMarkdown from 'react-markdown';
-import ChildTaskTable from './ChildTaskTable';
+import GeneralTaskChildrenTab from './GeneralTaskChildrenTab';
 
 interface GeneralTaskParams {
   id: string;
@@ -22,22 +27,12 @@ const generalTaskQuery = graphql`
         created
         updated
         agent_name
+        model_type
+        subtask_type
+        subtask_count
+        subtask_result
         children {
           total_count
-          edges {
-            node {
-              child {
-                ... on RuptureGenerationTask {
-                  __typename
-                  id
-                  created
-                  duration
-                  state
-                  result
-                }
-              }
-            }
-          }
         }
       }
     }
@@ -56,35 +51,57 @@ const GeneralTask: React.FC = () => {
     );
   }
 
-  const createdDate = data?.node?.created ? format(new Date(data?.node?.created as string), 'PPPppp') : '-';
-  const updatedDate = data?.node?.updated ? format(new Date(data?.node?.updated as string), 'PPPppp') : '-';
-  const childTasks = data?.node?.children?.edges?.map((e) => e?.node?.child);
+  const createdDate = data?.node?.created ? format(new Date(data?.node?.created as string), 'PPPppp') : '';
+  const updatedDate = data?.node?.updated ? format(new Date(data?.node?.updated as string), 'PPPppp') : '';
 
   return (
     <>
       <Typography variant="h5" gutterBottom>
         General Task: {data?.node?.title}
       </Typography>
+
       <Typography>
-        <strong>Agent name: </strong>
-        {data?.node?.agent_name}
+        <strong>Model type:</strong> {data?.node?.model_type}
       </Typography>
+      <Typography>
+        <strong>Subtask type:</strong> {data?.node?.subtask_type}
+      </Typography>
+      <Typography>
+        <strong>Subtask result:</strong> {data?.node?.subtask_result}
+      </Typography>
+      <Typography>
+        <strong>Subtask count:</strong> {data?.node?.subtask_count}
+      </Typography>
+      <Typography>
+        <strong>Agent name: </strong> {data?.node?.agent_name}
+      </Typography>
+
       <List dense>
         <ListItem>
           <ListItemText primary="Created" secondary={createdDate} />
         </ListItem>
-        <ListItem>
-          <ListItemText primary="Updated" secondary={updatedDate} />
-        </ListItem>
+        {updatedDate && (
+          <ListItem>
+            <ListItemText primary="Updated" secondary={updatedDate} />
+          </ListItem>
+        )}
       </List>
+
       <Typography>
         <strong>Description:</strong>
       </Typography>
       <ReactMarkdown>{data?.node?.description ?? ''}</ReactMarkdown>
-      <Typography>
-        <strong>Child tasks:</strong>
-      </Typography>
-      {!!data?.node?.children?.edges?.length && <ChildTaskTable data={childTasks} />}
+
+      <Accordion TransitionProps={{ unmountOnExit: true }}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="h6">Child tasks:</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <React.Suspense fallback={<CircularProgress />}>
+            <GeneralTaskChildrenTab id={id} />
+          </React.Suspense>
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 };
