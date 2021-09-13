a63a09b9b21257e4d1aefdda8bc437ec5ed8d171
0qy <linqianye333@gmail.com>
2021-09-13 16:09:43 +1200

    general task tab layout;

--- src/components/GeneralTask/GeneralTask.tsx
+++ src/components/GeneralTask/GeneralTask.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'babel-plugin-relay/macro';
 import { useParams } from 'react-router-dom';
 import { useLazyLoadQuery } from 'react-relay/hooks';
-import { Typography, CircularProgress } from '@material-ui/core';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Typography, CircularProgress, makeStyles, Theme, Tabs, Tab, Box } from '@material-ui/core';
 
 import { GeneralTaskQuery } from './__generated__/GeneralTaskQuery.graphql';
-import { format } from 'date-fns';
-import ReactMarkdown from 'react-markdown';
 import GeneralTaskChildrenTab from './GeneralTaskChildrenTab';
-import KeyValueListTable from './KeyValueListTable';
+import GeneralTaskDetailTab from './GeneralTaskDetailTab';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+  },
+  tabPanel: {
+    width: '80%',
+    padding: theme.spacing(2),
+  },
+  tab: {
+    width: '20%',
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
 interface GeneralTaskParams {
   id: string;
 }
@@ -24,6 +33,79 @@ interface KeyValueListPair {
 }
 
 type KeyValueListPairs = readonly (KeyValueListPair | null)[];
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  tab: number;
+}
+
+const GeneralTask: React.FC = () => {
+  const classes = useStyles();
+  const [tab, setTab] = useState(0);
+  const { id } = useParams<GeneralTaskParams>();
+  const data = useLazyLoadQuery<GeneralTaskQuery>(generalTaskQuery, { id });
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTab(newValue);
+  };
+
+  const TabPanel = (props: TabPanelProps) => {
+    const { children, tab, index } = props;
+    return (
+      <div
+        className={classes.tabPanel}
+        role="tabpanel"
+        hidden={tab !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+      >
+        {tab === index && <Box p={3}>{children}</Box>}
+      </div>
+    );
+  };
+
+  if (!data?.node) {
+    return (
+      <Typography variant="h5" gutterBottom>
+        General Task: Id Not Found
+      </Typography>
+    );
+  }
+
+  const sweptArguments = data?.node?.swept_arguments ?? [''];
+  const argumentLists = data?.node?.argument_lists ?? [];
+
+  const sweepsList = (arg_lists: KeyValueListPairs, sweeps: readonly (string | null)[]) => {
+    if (arg_lists) return arg_lists.filter((el) => sweeps.includes(el ? el.k : ''));
+    return [];
+  };
+
+  return (
+    <>
+      <Box className={classes.root}>
+        <Tabs orientation="vertical" value={tab} onChange={handleTabChange}>
+          <Tab label="Details" id="vertical-tab-0" aria-controls="vertical-tabpannel-0" className={classes.tab} />
+          <Tab label="Child Tasks" id="vertical-tab-1" aria-controls="vertical-tabpannel-1" className={classes.tab} />
+        </Tabs>
+        <TabPanel tab={tab} index={0}>
+          <GeneralTaskDetailTab sweepArgs={sweepsList(argumentLists, sweptArguments)} data={data} />
+        </TabPanel>
+        <TabPanel tab={tab} index={1}>
+          <Typography>
+            <strong>Child tasks</strong>
+          </Typography>
+          <React.Suspense fallback={<CircularProgress />}>
+            <GeneralTaskChildrenTab sweepArgs={sweepsList(argumentLists, sweptArguments)} id={id} />
+          </React.Suspense>
+        </TabPanel>
+      </Box>
+    </>
+  );
+};
+
+export default GeneralTask;
 
 const generalTaskQuery = graphql`
   query GeneralTaskQuery($id: ID!) {
@@ -52,110 +134,3 @@ const generalTaskQuery = graphql`
     }
   }
 `;
-
-const GeneralTask: React.FC = () => {
-  const { id } = useParams<GeneralTaskParams>();
-  const data = useLazyLoadQuery<GeneralTaskQuery>(generalTaskQuery, { id });
-
-  if (!data?.node) {
-    return (
-      <Typography variant="h5" gutterBottom>
-        General Task: Id Not Found
-      </Typography>
-    );
-  }
-
-  const createdDate = data?.node?.created ? format(new Date(data?.node?.created as string), 'PPPppp') : '';
-  const updatedDate = data?.node?.updated ? format(new Date(data?.node?.updated as string), 'PPPppp') : '';
-
-  const sweptArguments = data?.node?.swept_arguments ?? [''];
-  const argumentLists = data?.node?.argument_lists ?? [];
-
-  const sweepsList = (arg_lists: KeyValueListPairs, sweeps: readonly (string | null)[]) => {
-    if (arg_lists) return arg_lists.filter((el) => sweeps.includes(el ? el.k : ''));
-    return [];
-  };
-
-  return (
-    <>
-      <Typography variant="h5" gutterBottom>
-        General Task: {data?.node?.title}
-      </Typography>
-
-      <Typography>
-        <strong>Model type:</strong> {data?.node?.model_type}
-      </Typography>
-      <Typography>
-        <strong>Subtask type:</strong> {data?.node?.subtask_type}
-      </Typography>
-      <Typography>
-        <strong>Subtask result:</strong> {data?.node?.subtask_result}
-      </Typography>
-      <Typography>
-        <strong>Subtask count:</strong> {data?.node?.subtask_count}
-      </Typography>
-      <Typography>
-        <strong>Agent name: </strong> {data?.node?.agent_name}
-      </Typography>
-      <Typography>
-        <strong>Created: </strong> {createdDate}
-      </Typography>
-      {updatedDate && (
-        <Typography>
-          <strong>Updated: </strong> {updatedDate}
-        </Typography>
-      )}
-
-      <Typography>
-        <strong>Description:</strong>
-      </Typography>
-      <ReactMarkdown>{data?.node?.description ?? ''}</ReactMarkdown>
-      {data?.node?.notes && (
-        <span>
-          <Typography>
-            <strong>Notes:</strong>
-          </Typography>
-          <ReactMarkdown>{data?.node?.notes}</ReactMarkdown>
-        </span>
-      )}
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>
-            <strong>Swept Arguments</strong>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          {data?.node?.argument_lists && (
-            <KeyValueListTable header={null} data={sweepsList(argumentLists, sweptArguments)} />
-          )}
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>
-            <strong>All Arguments</strong>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          {data?.node?.argument_lists && <KeyValueListTable header={null} data={data?.node?.argument_lists} />}
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion TransitionProps={{ unmountOnExit: true }}>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>
-            <strong>Child tasks</strong>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <React.Suspense fallback={<CircularProgress />}>
-            <GeneralTaskChildrenTab sweepArgs={sweepsList(argumentLists, sweptArguments)} id={id} />
-          </React.Suspense>
-        </AccordionDetails>
-      </Accordion>
-    </>
-  );
-};
-
-export default GeneralTask;
