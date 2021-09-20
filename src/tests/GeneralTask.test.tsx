4219096bb8099b71cbebc65aa2688b9d368da88a
0qy <linqianye333@gmail.com>
2021-09-20 15:17:39 +1200

    testing general task component with mocking children

--- src/tests/GeneralTask.test.tsx
+++ src/tests/GeneralTask.test.tsx
@@ -1,31 +1,38 @@
 import React, { Suspense } from 'react';
-import { createMockEnvironment, MockPayloadGenerator, RelayMockEnvironment } from 'relay-test-utils';
-import GeneralTask from '../components/generalTask/GeneralTask';
 import ReactRouter from 'react-router';
-import { RelayEnvironmentProvider } from 'react-relay/hooks';
-import { cleanup, render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import { createMockEnvironment, MockPayloadGenerator, RelayMockEnvironment } from 'relay-test-utils';
+
+import GeneralTask from '../components/generalTask/GeneralTask';
 
 const mockResolver = {
   GeneralTask: () => ({
-    created: '2021-05-19T07:04:42.283510+00:00',
-    updated: '2021-05-19T07:04:42.283510+00:00',
-    children: {
-      total_count: 1,
-      edges: [
-        {
-          node: {
-            child: {
-              __typename: 'RuptureGenerationTask',
-              id: 'RGT-id',
-              created: '2021-05-19T07:04:42.283510+00:00',
-              duration: 1000,
-              state: 'DONE',
-              result: 'SUCCESS',
-            },
+    data: {
+      node: {
+        id: '1234',
+        title: 'mock title',
+        description: '',
+        notes: null,
+        created: '2021-09-14T00:19:24.130022+00:00',
+        updated: '2021-09-14T00:19:24.130022+00:00',
+        agent_name: 'mock name',
+        model_type: 'mock model type',
+        subtask_type: 'INVERSION',
+        subtask_count: 1,
+        subtask_result: null,
+        argument_lists: [
+          {
+            k: 'mock_k_1',
+            v: ['0'],
           },
-        },
-      ],
+          {
+            k: 'mock_k_2',
+            v: ['1'],
+          },
+        ],
+      },
     },
   }),
 };
@@ -42,20 +49,50 @@ const setup = (environment: RelayMockEnvironment) => {
   );
 };
 
+jest.mock('date-fns', () => ({
+  format: jest.fn(),
+}));
+
+jest.mock('../components/generalTask/GeneralTaskDetailTab', () => {
+  return function MockGeneralTaskDetailTab() {
+    return <div>detail tab component</div>;
+  };
+});
+
+jest.mock('../components/generalTask/GeneralTaskChildrenTab', () => {
+  return function MockGeneralTaskChildrenTab() {
+    return <div>children tab component</div>;
+  };
+});
+
 describe('GeneralTask component', () => {
   afterEach(() => {
     cleanup();
   });
 
-  it.todo('displays a GeneralTask using mock graphql payload');
-  // , async () => {
-  //   jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '1234' });
-  //   const environment = createMockEnvironment();
-  //   environment.mock.queueOperationResolver((operation) => MockPayloadGenerator.generate(operation, mockResolver));
+  it('displays GeneralTask with tabs using mock graphql payload', async () => {
+    jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '1234 ' });
+    const environment = createMockEnvironment();
 
-  //   const { findByText } = setup(environment);
-  //   expect(await findByText('[more]')).toHaveAttribute('href', '/RuptureGenerationTask/RGT-id');
-  // });
+    environment.mock.queueOperationResolver((operation) => MockPayloadGenerator.generate(operation, mockResolver));
+
+    const { findByText } = setup(environment);
+    expect(await findByText('Details')).toBeInTheDocument();
+    expect(await findByText('Child Tasks')).toBeInTheDocument();
+    expect(await findByText('detail tab component')).toBeInTheDocument();
+  });
+
+  it('displays Child tasks tab when clicked', async () => {
+    jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '1234 ' });
+    const environment = createMockEnvironment();
+
+    environment.mock.queueOperationResolver((operation) => MockPayloadGenerator.generate(operation, mockResolver));
+
+    const { getByText, findByText } = setup(environment);
+    fireEvent.click(getByText('Child Tasks'));
+
+    expect(await findByText('children tab component')).toBeInTheDocument();
+  });
 
   it('displays not found when no matching id', async () => {
     jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '1234' });
