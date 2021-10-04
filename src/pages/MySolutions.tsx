5cc1dcb1a830cb76d2ce551090ac40a18fe7ddd7
0qy <linqianye333@gmail.com>
2021-10-05 10:50:07 +1300

    refactor my solution page

--- src/pages/MySolutions.tsx
+++ src/pages/MySolutions.tsx
@@ -2,57 +2,39 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useLazyLoadQuery } from 'react-relay';
 import { Button, Typography } from '@material-ui/core';
 import { graphql } from 'babel-plugin-relay/macro';
+
 import DiagnosticReportControls from '../components/diagnosticReportView/DiagnosticReportControls';
 import GeneralTaskDetailDrawer from '../components/diagnosticReportView/GeneralTaskDetailDrawer';
 import DiagnosticReportCard from '../components/diagnosticReportView/DiagnosticReportCard';
 import MySolutionsList from '../components/mySolutions/MySolutionsList';
 import ControlsBar from '../components/common/ControlsBar';
 import LocalStorageContext from '../contexts/localStorage';
-import { SolutionItem } from '../interfaces/mySolutions';
-import { GeneralTaskDetails, ReportItems } from '../interfaces/diagnosticReport';
+import { GeneralTaskDetails } from '../interfaces/diagnosticReport';
 import { MySolutionsQuery } from './__generated__/MySolutionsQuery.graphql';
 import { diagnosticReportViewOptions as options } from '../constants/diagnosticReport';
-import { getGeneralTaskDetails } from '../service/mySolution.service';
+import {
+  getGeneralTaskDetails,
+  getMySolutionIdsArray,
+  getReportItems,
+  validateListItems,
+} from '../service/mySolution.service';
 
 const MySolutions: React.FC = () => {
-  const { ISFavourites } = useContext(LocalStorageContext);
   const [showList, setShowList] = useState(true);
   const [viewOptions, setViewOptions] = useState<string[]>([options[0].finalPath]);
   const [openDrawer, setOpenDrawer] = useState(false);
   const [currentImage, setCurrentImage] = useState<number>(0);
 
-  const id: string[] = [];
-  for (const inversionSolution in ISFavourites) {
-    id.push(ISFavourites[inversionSolution].producedBy);
-  }
-
+  const { ISFavourites } = useContext(LocalStorageContext);
+  const id = getMySolutionIdsArray(ISFavourites);
   const data = useLazyLoadQuery<MySolutionsQuery>(mySolutionsQuery, { id });
-  const automationTasks = data?.nodes?.result?.edges.map((e) => e?.node) ?? [];
-  const listItems: SolutionItem[] = [];
-  const reportItems: ReportItems[] = [];
-
-  automationTasks.map((item) => {
-    if (item && item !== null && item.__typename === 'AutomationTask' && item.inversion_solution !== null) {
-      listItems.push(item);
-    }
-  });
-
-  listItems.map((task) => {
-    const newMeta = task.inversion_solution?.meta ?? [];
-    const validatedTask: ReportItems = {
-      __typename: 'AutomationTask',
-      id: task.id,
-      inversion_solution: {
-        id: task.inversion_solution?.id as string,
-        meta: [...newMeta],
-      },
-    };
-    reportItems.push(validatedTask);
-  });
+  const listItems = validateListItems(data);
+  const reportItems = getReportItems(listItems);
 
   const [currentGeneralTask, setCurrentGeneralTask] = useState<GeneralTaskDetails>(
     getGeneralTaskDetails(listItems, reportItems, 0),
   );
+
   const handleChange = (event: React.ChangeEvent<{ value: unknown; name?: string | undefined }>) => {
     const newValue = (event.target.value as string[]) || [];
     setViewOptions(newValue);
--- src/service/mySolution.service.ts
+++ src/service/mySolution.service.ts
@@ -1,5 +1,9 @@
+import { LocalStorageChanged } from '@rehooks/local-storage/lib/local-storage-events';
+import { report } from 'process';
 import { GeneralTaskDetails, ReportItems } from '../interfaces/diagnosticReport';
+import { LocalStorageInstance } from '../interfaces/localStorage';
 import { SolutionItem } from '../interfaces/mySolutions';
+import { MySolutionsQueryResponse } from '../pages/__generated__/MySolutionsQuery.graphql';
 
 export const getGeneralTaskDetails = (
   listItems: SolutionItem[],
@@ -18,4 +22,40 @@ export const getGeneralTaskDetails = (
     swept_arguments: (currentTaskParent?.swept_arguments as string[]) ?? [],
     argument_lists: currentTaskParent?.argument_lists ?? [],
   };
+};
+
+export const validateListItems = (data: MySolutionsQueryResponse): SolutionItem[] => {
+  const automationTasks = data?.nodes?.result?.edges.map((e) => e?.node) ?? [];
+  const listItems: SolutionItem[] = [];
+  automationTasks.map((item) => {
+    if (item && item !== null && item.__typename === 'AutomationTask' && item.inversion_solution !== null) {
+      listItems.push(item);
+    }
+  });
+  return listItems;
+};
+
+export const getReportItems = (listItems: SolutionItem[]): ReportItems[] => {
+  const reportItems: ReportItems[] = [];
+  listItems.map((task) => {
+    const newMeta = task.inversion_solution?.meta ?? [];
+    const validatedTask: ReportItems = {
+      __typename: 'AutomationTask',
+      id: task.id,
+      inversion_solution: {
+        id: task.inversion_solution?.id as string,
+        meta: [...newMeta],
+      },
+    };
+    reportItems.push(validatedTask);
+  });
+  return reportItems;
+};
+
+export const getMySolutionIdsArray = (ISFavourites: LocalStorageInstance): string[] => {
+  const ids: string[] = [];
+  for (const inversionSolution in ISFavourites) {
+    ids.push(ISFavourites[inversionSolution].producedBy);
+  }
+  return ids;
 };
