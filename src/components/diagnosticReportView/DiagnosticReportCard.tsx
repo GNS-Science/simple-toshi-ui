9f705b41fac0b5977f9f46e1c54eb2d6d4326884
0qy <linqianye333@gmail.com>
2021-10-07 11:57:59 +1300

    fix mistake

--- src/components/diagnosticReportView/DiagnosticReportCard.tsx
+++ src/components/diagnosticReportView/DiagnosticReportCard.tsx
@@ -111,7 +111,7 @@ const DiagnosticReportCard: React.FC<DiagnosticReportCardProps> = ({
             <Typography>
               {currentImage + 1}&nbsp;of&nbsp;{automationTasks.length}
             </Typography>
-            <Tooltip title="use < > comparison operators to navigate">
+            <Tooltip title="use (<,) (>.) or arrow keys to navigate">
               <IconButton
                 className={classes.button}
                 color="primary"
