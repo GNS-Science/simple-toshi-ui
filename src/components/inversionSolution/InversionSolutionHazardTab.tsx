7c1d27d2bd82651a896ca2252b231a39a605e81b
QY <linqianye@tutanota.com>
2021-10-22 13:15:50 +1300

    add hazard tab query

--- src/components/inversionSolution/InversionSolutionHazardTab.tsx
+++ src/components/inversionSolution/InversionSolutionHazardTab.tsx
@@ -1,54 +1,18 @@
 import { Typography, Box, Card } from '@material-ui/core';
 import { graphql } from 'babel-plugin-relay/macro';
 import React from 'react';
-// eslint-disable-next-line
-import { PreloadedQuery, usePreloadedQuery } from 'react-relay';
-import { LatLngExpression } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { Map, TileLayer } from 'react-leaflet';
-// import { formatBytes } from './FileDetail';
+import { useLazyLoadQuery } from 'react-relay';
 import { InversionSolutionHazardTabQuery } from './__generated__/InversionSolutionHazardTabQuery.graphql';
-// import KeyValueTable from './KeyValueTable';
-
-export const inversionSolutionHazardTabQuery = graphql`
-  query InversionSolutionHazardTabQuery($id: ID!) {
-    node(id: $id) {
-      ... on InversionSolution {
-        id
-        meta {
-          k
-          v
-        }
-        hazard_table {
-          id
-          name
-          column_types
-          column_headers
-          rows
-        }
-      }
-    }
-  }
-`;
 
 interface InversionSolutionHazardTabProps {
-  queryRef: PreloadedQuery<InversionSolutionHazardTabQuery, Record<string, unknown>>;
+  id: string;
 }
 
 const InversionSolutionHazardTab: React.FC<InversionSolutionHazardTabProps> = ({
-  // eslint-disable-next-line
-  queryRef,
+  id,
 }: InversionSolutionHazardTabProps) => {
-  // const data = usePreloadedQuery<InversionSolutionHazardTabQuery>(inversionSolutionHazardTabQuery, queryRef);
-
-  // Default coordinates set to Masterton station
-  const nz_centre: LatLngExpression = [-40.946, 174.167];
-  const zoom = 5;
-
-  const provider_url = 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}';
-  const provider_attibution =
-    'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri';
-
+  const data = useLazyLoadQuery<InversionSolutionHazardTabQuery>(inversionSolutionHazardTabQuery, { id });
+  console.log(data);
   return (
     <>
       <Box>
@@ -57,14 +21,6 @@ const InversionSolutionHazardTab: React.FC<InversionSolutionHazardTabProps> = ({
             <strong>Hazard:</strong>
             {/*{data?.node?.hazard_table?.name}*/}
           </Typography>
-          <Map center={nz_centre} zoom={zoom} scrollWheelZoom={false} style={{ height: '700px' }}>
-            <TileLayer attribution={provider_attibution} url={provider_url} />
-            {/*<Marker position={position}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>*/}
-          </Map>
         </Card>
       </Box>
     </>
@@ -72,3 +28,24 @@ const InversionSolutionHazardTab: React.FC<InversionSolutionHazardTabProps> = ({
 };
 
 export default InversionSolutionHazardTab;
+
+export const inversionSolutionHazardTabQuery = graphql`
+  query InversionSolutionHazardTabQuery($id: ID!) {
+    node(id: $id) {
+      ... on Table {
+        id
+        name
+        created
+        table_type
+        object_id
+        column_headers
+        column_types
+        rows
+        dimensions {
+          k
+          v
+        }
+      }
+    }
+  }
+`;
