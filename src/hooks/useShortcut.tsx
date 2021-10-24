d61dc8e66d423af06bab93243476a8ed1d52f4d9
Wooneh <sam.woon@hotmail.com>
2021-10-24 23:04:00 +1300

    clean up event listeners

--- src/hooks/useShortcut.tsx
+++ src/hooks/useShortcut.tsx
@@ -3,14 +3,14 @@ import { useHistory } from 'react-router';
 
 export function useShortcut(callback: () => void, keyCodes: string[]): void {
   const history = useHistory();
-  const handler = (event: KeyboardEvent) => {
-    const searchParams = new URLSearchParams(history.location.search);
-    if (keyCodes.includes(event.key.toLowerCase()) && !searchParams.get('modal')) {
-      callback();
-    }
-  };
 
   React.useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      const searchParams = new URLSearchParams(history.location.search);
+      if (keyCodes.includes(event.key.toLowerCase()) && !searchParams.get('modal')) {
+        callback();
+      }
+    };
     window.addEventListener('keydown', handler);
     return () => {
       window.removeEventListener('keydown', handler);
