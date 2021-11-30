b049a0ff1b7183f1537da9a8a1e6bb040b70dbf1
QY <linqianye@tutanota.com>
2021-12-01 12:59:23 +1300

    update theme with GNS red

--- src/theme.ts
+++ src/theme.ts
@@ -1,24 +1,22 @@
-import { createTheme, adaptV4Theme } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 // A custom theme for this app
-const theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      primary: {
-        main: '#556cd6',
-      },
-      secondary: {
-        main: '#19857b',
-      },
-      error: {
-        main: red.A400,
-      },
-      background: {
-        default: '#fff',
-      },
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#a30027',
     },
-  }),
-);
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
 
 export default theme;
