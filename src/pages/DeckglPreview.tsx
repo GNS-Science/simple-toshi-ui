b86839ee71514cbfefaf7aeba9e6eb7ee4400138
QY <linqianye@tutanota.com>
2022-01-19 12:13:50 +1300

    add axios request

--- src/pages/DeckglPreview.tsx
+++ src/pages/DeckglPreview.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import { StaticMap } from 'react-map-gl';
 import { DeckGL } from '@deck.gl/react';
 import { LineLayer } from '@deck.gl/layers';
+import axios, { AxiosResponse } from 'axios';
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
@@ -11,11 +12,35 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
   bearing: 0,
 };
-
+const solvisEndpoint = process.env.REACT_APP_SOLVIS_ENDPOINT as string;
 const data = [{ sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }];
 
 const DeckglPreview: React.FC = () => {
   const layers = [new LineLayer({ id: 'line-layer', data })];
+
+  useEffect(() => {
+    axios
+      .get(`${solvisEndpoint}/locations/`, {
+        headers: {
+          'x-api-key': process.env.REACT_APP_SOLVIS_API_KEY as string,
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      });
+
+    axios
+      .get(`${solvisEndpoint}/radii/`, {
+        headers: {
+          'x-api-key': process.env.REACT_APP_SOLVIS_API_KEY as string,
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .then((response) => {
+        console.log('radii', response);
+      });
+  });
 
   return (
     <DeckGL layers={layers} initialViewState={INITIAL_VIEW_STATE} controller={true}>
