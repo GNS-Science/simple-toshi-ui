ff5826f7c59052e963bc46c08602f687e234d617
QY <linqianye@tutanota.com>
2022-01-25 11:07:11 +1300

    allow any

--- src/pages/SolutionAnalysisPreview.tsx
+++ src/pages/SolutionAnalysisPreview.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState, useEffect } from 'react';
 import { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
