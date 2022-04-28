3f55305eeadedd8d21823b5c5102b945364184af
QY <linqianye@tutanota.com>
2022-04-28 16:25:45 +1200

    relay fix rebase err

--- src/pages/__generated__/InversionSolutionNrmlQuery.graphql.ts
+++ src/pages/__generated__/InversionSolutionNrmlQuery.graphql.ts
@@ -2,41 +2,38 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from 'relay-runtime';
+import { ConcreteRequest } from "relay-runtime";
 
 export type InversionSolutionNrmlQueryVariables = {
-  id: string;
+    id: string;
 };
 export type InversionSolutionNrmlQueryResponse = {
-  readonly node:
-    | (
-        | {
-            readonly __typename: 'InversionSolutionNrml';
+    readonly node: ({
+        readonly __typename: "InversionSolutionNrml";
+        readonly id: string;
+        readonly source_solution: {
             readonly id: string;
-            readonly source_solution: {
-              readonly id: string;
-            } | null;
-            readonly meta: ReadonlyArray<{
-              readonly k: string | null;
-              readonly v: string | null;
-            } | null> | null;
-            readonly file_size: number | null;
-            readonly md5_digest: string | null;
-            readonly file_name: string | null;
-            readonly file_url: string | null;
-          }
-        | {
-            /*This will never be '%other', but we need some
+        } | null;
+        readonly meta: ReadonlyArray<{
+            readonly k: string | null;
+            readonly v: string | null;
+        } | null> | null;
+        readonly file_size: number | null;
+        readonly md5_digest: string | null;
+        readonly file_name: string | null;
+        readonly file_url: string | null;
+    } | {
+        /*This will never be '%other', but we need some
         value in case none of the concrete values match.*/
-            readonly __typename: '%other';
-          }
-      )
-    | null;
+        readonly __typename: "%other";
+    }) | null;
 };
 export type InversionSolutionNrmlQuery = {
-  readonly response: InversionSolutionNrmlQueryResponse;
-  readonly variables: InversionSolutionNrmlQueryVariables;
+    readonly response: InversionSolutionNrmlQueryResponse;
+    readonly variables: InversionSolutionNrmlQueryVariables;
 };
+
+
 
 /*
 query InversionSolutionNrmlQuery(
@@ -63,171 +60,180 @@ query InversionSolutionNrmlQuery(
 }
 */
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "InversionSolution",
+  "kind": "LinkedField",
+  "name": "source_solution",
+  "plural": false,
+  "selections": [
+    (v3/*: any*/)
+  ],
+  "storageKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "KeyValuePair",
+  "kind": "LinkedField",
+  "name": "meta",
+  "plural": true,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "k",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "v",
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+},
+v6 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "file_size",
+  "storageKey": null
+},
+v7 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "md5_digest",
+  "storageKey": null
+},
+v8 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "file_name",
+  "storageKey": null
+},
+v9 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "file_url",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "InversionSolutionNrmlQuery",
+    "selections": [
       {
-        defaultValue: null,
-        kind: 'LocalArgument',
-        name: 'id',
-      },
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v2/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              (v3/*: any*/),
+              (v4/*: any*/),
+              (v5/*: any*/),
+              (v6/*: any*/),
+              (v7/*: any*/),
+              (v8/*: any*/),
+              (v9/*: any*/)
+            ],
+            "type": "InversionSolutionNrml",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
     ],
-    v1 = [
+    "type": "QueryRoot",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "InversionSolutionNrmlQuery",
+    "selections": [
       {
-        kind: 'Variable',
-        name: 'id',
-        variableName: 'id',
-      },
-    ],
-    v2 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: '__typename',
-      storageKey: null,
-    },
-    v3 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'id',
-      storageKey: null,
-    },
-    v4 = {
-      alias: null,
-      args: null,
-      concreteType: 'InversionSolution',
-      kind: 'LinkedField',
-      name: 'source_solution',
-      plural: false,
-      selections: [v3 /*: any*/],
-      storageKey: null,
-    },
-    v5 = {
-      alias: null,
-      args: null,
-      concreteType: 'KeyValuePair',
-      kind: 'LinkedField',
-      name: 'meta',
-      plural: true,
-      selections: [
-        {
-          alias: null,
-          args: null,
-          kind: 'ScalarField',
-          name: 'k',
-          storageKey: null,
-        },
-        {
-          alias: null,
-          args: null,
-          kind: 'ScalarField',
-          name: 'v',
-          storageKey: null,
-        },
-      ],
-      storageKey: null,
-    },
-    v6 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'file_size',
-      storageKey: null,
-    },
-    v7 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'md5_digest',
-      storageKey: null,
-    },
-    v8 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'file_name',
-      storageKey: null,
-    },
-    v9 = {
-      alias: null,
-      args: null,
-      kind: 'ScalarField',
-      name: 'file_url',
-      storageKey: null,
-    };
-  return {
-    fragment: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Fragment',
-      metadata: null,
-      name: 'InversionSolutionNrmlQuery',
-      selections: [
-        {
-          alias: null,
-          args: v1 /*: any*/,
-          concreteType: null,
-          kind: 'LinkedField',
-          name: 'node',
-          plural: false,
-          selections: [
-            v2 /*: any*/,
-            {
-              kind: 'InlineFragment',
-              selections: [
-                v3 /*: any*/,
-                v4 /*: any*/,
-                v5 /*: any*/,
-                v6 /*: any*/,
-                v7 /*: any*/,
-                v8 /*: any*/,
-                v9 /*: any*/,
-              ],
-              type: 'InversionSolutionNrml',
-              abstractKey: null,
-            },
-          ],
-          storageKey: null,
-        },
-      ],
-      type: 'QueryRoot',
-      abstractKey: null,
-    },
-    kind: 'Request',
-    operation: {
-      argumentDefinitions: v0 /*: any*/,
-      kind: 'Operation',
-      name: 'InversionSolutionNrmlQuery',
-      selections: [
-        {
-          alias: null,
-          args: v1 /*: any*/,
-          concreteType: null,
-          kind: 'LinkedField',
-          name: 'node',
-          plural: false,
-          selections: [
-            v2 /*: any*/,
-            v3 /*: any*/,
-            {
-              kind: 'InlineFragment',
-              selections: [v4 /*: any*/, v5 /*: any*/, v6 /*: any*/, v7 /*: any*/, v8 /*: any*/, v9 /*: any*/],
-              type: 'InversionSolutionNrml',
-              abstractKey: null,
-            },
-          ],
-          storageKey: null,
-        },
-      ],
-    },
-    params: {
-      cacheID: 'ed836dd91ae835a0007311df0fc42873',
-      id: null,
-      metadata: {},
-      name: 'InversionSolutionNrmlQuery',
-      operationKind: 'query',
-      text: 'query InversionSolutionNrmlQuery(\n  $id: ID!\n) {\n  node(id: $id) {\n    __typename\n    ... on InversionSolutionNrml {\n      id\n      source_solution {\n        id\n      }\n      meta {\n        k\n        v\n      }\n      file_size\n      md5_digest\n      file_name\n      file_url\n    }\n    id\n  }\n}\n',
-    },
-  };
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v2/*: any*/),
+          (v3/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              (v4/*: any*/),
+              (v5/*: any*/),
+              (v6/*: any*/),
+              (v7/*: any*/),
+              (v8/*: any*/),
+              (v9/*: any*/)
+            ],
+            "type": "InversionSolutionNrml",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "ed836dd91ae835a0007311df0fc42873",
+    "id": null,
+    "metadata": {},
+    "name": "InversionSolutionNrmlQuery",
+    "operationKind": "query",
+    "text": "query InversionSolutionNrmlQuery(\n  $id: ID!\n) {\n  node(id: $id) {\n    __typename\n    ... on InversionSolutionNrml {\n      id\n      source_solution {\n        id\n      }\n      meta {\n        k\n        v\n      }\n      file_size\n      md5_digest\n      file_name\n      file_url\n    }\n    id\n  }\n}\n"
+  }
+};
 })();
 (node as any).hash = 'f226e164e47d5e03275bbc57adfa1b58';
 export default node;
