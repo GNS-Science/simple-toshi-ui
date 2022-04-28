be0d3486163274d1a5d66460f31a3abfc001b393
Qian-Ye_Lin <52944295+0qy@users.noreply.github.com>
2022-04-28 15:52:47 +1200

    Feature/345 inversion solution nrml objects (#351)
    * add route
    * update import
    * update path
    * InversionSolutionNrml page displays correct information;
    * refactor
    * fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,7 @@ import Loading from './components/common/Loading';
 import theme from './theme';
 import { ThemeProvider } from '@mui/material';
 import { parentViewsOptions } from './constants/parentFault';
+import InversionSolutionNrml from './pages/InversionSolutionNrml';
 
 // Immediately load the query as our app starts. For a real app, we'd move this
 // into our routing configuration, preloading data as we transition to new routes.
@@ -136,6 +137,9 @@ function AppRoot(props: { environment?: Environment }): React.ReactElement {
                   </Route>
                   <Route path="/HazardSolution/:id">
                     <OpenquakeHazardSolution />
+                  </Route>
+                  <Route path="/InversionSolutionNrml/:id">
+                    <InversionSolutionNrml />
                   </Route>
                   <Route path="/Find/:id?">
                     <Find />
